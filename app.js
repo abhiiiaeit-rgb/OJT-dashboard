62cc0746660507e5e4173b9a8eb5fcd7f5fa15c9
abhiiiaeit-rgb <abhiiiaeit@gmail.com>
2026-04-14 14:51:25 +0530

    Enhance data processing and display in app.js

--- app.js
+++ app.js
@@ -3,20 +3,60 @@ const API_URL = "https://opensheet.elk.sh/1DFsrdpwH5XSdNcmyXCqKE-FRFSMXQKxRj2d0F
 fetch(API_URL)
   .then(res => res.json())
   .then(data => {
+
     console.log(data);
 
     const container = document.getElementById("data");
 
+    let totalHC = 0;
+    let totalAttrition = 0;
+    let trainerMap = {};
+
+    data.forEach(item => {
+
+      const hc = Number(item["Headcount Day 0"]) || 0;
+      const attr = Number((item["Attrition %"] || "0").replace("%",""));
+
+      totalHC += hc;
+      totalAttrition += attr;
+
+      const trainer = item["Trainer Name"];
+
+      if (!trainerMap[trainer]) {
+        trainerMap[trainer] = 0;
+      }
+      trainerMap[trainer] += hc;
+    });
+
+    // KPI Updates
+    document.getElementById("totalBatches").innerText = data.length;
+    document.getElementById("totalHC").innerText = totalHC;
+    document.getElementById("avgAttrition").innerText =
+      (totalAttrition / data.length).toFixed(1) + "%";
+
+    // Trainer Chart
+    new Chart(document.getElementById("trainerChart"), {
+      type: "bar",
+      data: {
+        labels: Object.keys(trainerMap),
+        datasets: [{
+          label: "Headcount by Trainer",
+          data: Object.values(trainerMap)
+        }]
+      }
+    });
+
+    // Cards
     container.innerHTML = data.map(item => `
-      <div style="border:1px solid #ddd; padding:12px; margin:10px; border-radius:8px;">
+      <div class="card">
         <h3>${item["Batches"]}</h3>
         <p><b>Trainer:</b> ${item["Trainer Name"]}</p>
         <p><b>Process:</b> ${item["Process"]}</p>
         <p><b>Month:</b> ${item["Month"]}</p>
-        <p><b>Headcount:</b> ${item["Headcount Day 0"]}</p>
-        <p><b>Attrition %:</b> ${item["Attrition %"]}</p>
-        <p><b>PKT %:</b> ${item["PKT Certification %"]}</p>
+        <p><b>HC:</b> ${item["Headcount Day 0"]}</p>
+        <p><b>Attrition:</b> ${item["Attrition %"]}</p>
       </div>
     `).join("");
+
   })
   .catch(err => console.error(err));
